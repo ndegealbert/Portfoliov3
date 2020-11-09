3fbcc38f0d3230fbf879323908a2023681ffe66c
albert <ndegealbert>
2020-11-09 00:32:31 -0500

    add resume  link

--- pages/navbar/nav.js
+++ pages/navbar/nav.js
@@ -5,9 +5,11 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import React from 'react';
+import React,{useState} from 'react';
 import  styles from  '../../styles/nav.module.css'
 import   { Link }   from  'react-scroll'
+
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -28,11 +30,10 @@ const useStyles = makeStyles((theme) => ({
 }))
 
 export default function Nav(){
-const classes = useStyles();
-
-  
+const classes = useStyles();  
    return (
        <div>
+      
         <AppBar className={classes.app} position="fixed">
         <Toolbar>
           <Typography variant="h6" className={classes.title}>
@@ -44,8 +45,8 @@ const classes = useStyles();
               
            <Link style={{color:"#C4C1E7"}}  to="about" smooth={true}   offset={-80}  duration={1000}>About</Link >   
            <Link  style={{color:"#C4C1E7"}} to="project" smooth={true} offset={-80} duration={1000}>Project</Link >
-           <Link  style={{color:"#C4C1E7"}} to="contact" smooth={true} offset={-80} duration={1000}>Contact</Link >          
-              <a className={styles.re} href="public/Ndege Albert.pdf" download>Resume</a>
+           <Link  style={{color:"#C4C1E7"}} to="contact" smooth={true} offset={-80} duration={1000}>Contact</Link >  
+              <a className={styles.re} href="https://portfolio-theta-black.vercel.app/public/resume.pdf" download>Resume</a>
             </div>
   
           
